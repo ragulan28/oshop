64b01eb9c6c3e6e8a29aebe38e969cda2ce11a6d
ragulan28 <Ragulan28@gmail.com>
2018-03-03 09:16:48 +0530

    Add auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { environment } from './../environments/environment';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import{AngularFireModule} from 'angularfire2';
-import{ AngularFireDatabaseModule} from 'angularfire2/database'
-import{ AngularFireAuthModule} from 'angularfire2/auth'
+import {environment} from './../environments/environment';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 import {RouterModule} from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { LoginComponent } from './login/login.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {AppComponent} from './app.component';
+import {ProductFormComponent} from './admin/product-form/product-form.component';
+import {BsNavbarComponent} from './bs-navbar/bs-navbar.component';
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent} from './products/products.component';
+import {ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {CheckOutComponent} from './check-out/check-out.component';
+import {OrderSuccessComponent} from './order-success/order-success.component';
+import {MyOrdersComponent} from './my-orders/my-orders.component';
+import {AdminProductsComponent} from './admin/admin-products/admin-products.component';
+import {LoginComponent} from './login/login.component';
+import {AdminOrdersComponent} from './admin/admin-orders/admin-orders.component';
 import {AuthService} from './auth.service';
+import {AuthGuardService} from './auth-guard.service';
 
 
 @NgModule({
@@ -44,21 +45,24 @@ import {AuthService} from './auth.service';
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      {path:'',component:HomeComponent},
-      {path:'products',component:ProductsComponent},
-      {path:'shopping-cart',component:ShoppingCartComponent},
-      {path:'my/orders',component:MyOrdersComponent},
-      {path:'check-out',component:CheckOutComponent},
-      {path:'order-success',component:OrderSuccessComponent},
-      {path:'login',component:LoginComponent},
-      {path:'admin/products',component:AdminProductsComponent},
-      {path:'admin/orders',component:AdminOrdersComponent}
+        {path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuardService]},
+        {path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuardService]},
+        {path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuardService]},
+        {path: 'shopping-cart', component: ShoppingCartComponent},
+
+        {path: '', component: HomeComponent},
+        {path: 'products', component: ProductsComponent},
+        {path: 'login', component: LoginComponent},
+
+        {path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuardService]},
+        {path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuardService]}
       ]
     )
   ],
   providers: [
-    AuthService
+    AuthService, AuthGuardService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
